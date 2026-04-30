d25c61b1b2c34d18cfb31f8683cd9fa98c65a898
Vivik1812 <an.verde@duocuc.cl>
2026-04-30 19:55:30 -0400

    First Commit

---
+++ src/main/java/com/publicaciones/publicaciones_service/PublicacionesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.publicaciones.publicaciones_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicacionesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicacionesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicaciones/publicaciones_service/PublicacionesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicaciones.publicaciones_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicacionesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
