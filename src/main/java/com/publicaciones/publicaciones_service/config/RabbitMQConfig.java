4104990c9be673570327af26585cf11c4462dc6e
Vivik1812 <an.verde@duocuc.cl>
2026-05-14 00:43:59 -0400

    fix: Service funcional(Listo para Render)

--- src/main/java/com/publicaciones/publicaciones_service/config/RabbitMQConfig.java
+++ src/main/java/com/publicaciones/publicaciones_service/config/RabbitMQConfig.java
@@ -4,9 +4,12 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @Configuration
 public class RabbitMQConfig {
@@ -31,8 +34,14 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public Jackson2JsonMessageConverter converter() {
-        return new Jackson2JsonMessageConverter();
+    public JacksonJsonMessageConverter converter(){
+        return new JacksonJsonMessageConverter();
     }
 
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(converter());
+        return template;
+    }
 }
